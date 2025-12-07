e7baa712d7488cda078384212bb8c2ddbff13fbf
Nizamuddin Shaikh <nizamudd.shaikhh@gmail.com>
2025-12-07 15:54:44 +0530

    First commit.

---
+++ src/main/java/com/shaikh/TemporalPocApplication.java
@@ -0,0 +1,13 @@
+package com.shaikh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaikh/TemporalPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaikh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
